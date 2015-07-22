13907588e75e8db07e1ede8fc9b157adfb0a46ec
Will McCutchen <will@mccutch.org>
2015-07-22 10:57:59 -0700

    factor out assignmentStep() and updateStep() funcs
    This should make testing corner cases a bit easier.

--- kmeans.go
+++ kmeans.go
@@ -22,33 +22,16 @@ func ClusterColors(k, maxIterations int, colors []color.Color) (*ColorPalette, e
 		return nil, fmt.Errorf("too few colors for k (%d < %d)", colorCount, k)
 	}
 
-	centroids := initializeCentroids(k, colors)
+	centroids := initializeStep(k, colors)
 	var clusters map[color.Color][]color.Color
+	var converged bool
 
 	// The algorithm isn't guaranteed to converge, so we put a limit on the
 	// number of attempts we will make.
 	var iterations int
 	for iterations = 0; iterations < maxIterations; iterations++ {
-		clusters = make(map[color.Color][]color.Color, k)
-
-		// Assign each color to the cluster of the closest centroid.
-		for _, x := range colors {
-			centroid := nearest(x, centroids)
-			clusters[centroid] = append(clusters[centroid], x)
-		}
-
-		// Pick new centroids from each cluster. If none of the centroids
-		// change, the clusters have stabilized and we're done.
-		converged := true
-		var newCentroids []color.Color
-		for centroid, cluster := range clusters {
-			newCentroid := findCentroid(cluster)
-			if newCentroid != centroid {
-				converged = false
-			}
-			newCentroids = append(newCentroids, newCentroid)
-		}
-		centroids = newCentroids
+		clusters = assignmentStep(centroids, colors)
+		converged, centroids = updateStep(clusters)
 		if converged {
 			break
 		}
@@ -65,17 +48,12 @@ func ClusterColors(k, maxIterations int, colors []color.Color) (*ColorPalette, e
 }
 
 // Generate the initial list of k centroids from the given list of colors.
-func initializeCentroids(k int, colors []color.Color) []color.Color {
-	// We just choose k random items from the list. Apparently, this is the
-	// "Forgy Method". TODO: Try the Random Partition method?
-	// https://en.wikipedia.org/wiki/K-means_clustering#Initialization_methods
-	//
-	// We take care to track the random indexes we've used to avoid picking the
-	// same color for multiple centroids in the case len(colors) is
-	// close to k.
+func initializeStep(k int, colors []color.Color) []color.Color {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	centroids := make([]color.Color, k)
 	colorCount := len(colors)
+	// Track random indexes we've used to avoid picking the same index for
+	// multiple centroids in the case len(colors) is close to k.
 	usedIndexes := make(map[int]bool, k)
 	var index int
 	for i := 0; i < k; i++ {
@@ -89,6 +67,31 @@ func initializeCentroids(k int, colors []color.Color) []color.Color {
 		centroids[i] = colors[index]
 	}
 	return centroids
+}
+
+// Assign each color to the cluster of the closest centroid.
+func assignmentStep(centroids, colors []color.Color) map[color.Color][]color.Color {
+	clusters := make(map[color.Color][]color.Color)
+	for _, x := range colors {
+		centroid := nearest(x, centroids)
+		clusters[centroid] = append(clusters[centroid], x)
+	}
+	return clusters
+}
+
+// Pick new centroids from each cluster. If none of the centroids change, the
+// clusters have stabilized and the algorithm has converged.
+func updateStep(clusters map[color.Color][]color.Color) (bool, []color.Color) {
+	converged := true
+	var newCentroids []color.Color
+	for centroid, cluster := range clusters {
+		newCentroid := findCentroid(cluster)
+		if newCentroid != centroid {
+			converged = false
+		}
+		newCentroids = append(newCentroids, newCentroid)
+	}
+	return converged, newCentroids
 }
 
 // Find the color closest to the mean of the given colors.
