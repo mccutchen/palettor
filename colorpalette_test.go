ce5dd3c29180e3a688ce9388ad8959fb1ebda307
Will McCutchen <will@mccutch.org>
2015-07-21 17:03:54 -0700

    tests for type ColorPalette and func FindPalette()

---
+++ colorpalette_test.go
@@ -0,0 +1,46 @@
+package palettor
+
+import (
+	"image/color"
+	"testing"
+)
+
+func TestColorPalette(t *testing.T) {
+	colorWeights := map[color.Color]float64{
+		black: 0.5,
+		white: 0.5,
+	}
+	iterations := 1
+	palette := &ColorPalette{
+		colorWeights: colorWeights,
+		iterations:   iterations,
+	}
+
+	if palette.Count() != len(colorWeights) {
+		t.Errorf("wrong number of colors in palette")
+	}
+
+	if palette.Iterations() != iterations {
+		t.Errorf("wrong number of iterations in palette")
+	}
+
+	if palette.Weight(black) != 0.5 {
+		t.Errorf("wrong weight for black")
+	}
+	if palette.Weight(red) != 0 {
+		t.Errorf("wrong weight for unknown color")
+	}
+
+	for _, color := range palette.Colors() {
+		found := false
+		for inputColor := range colorWeights {
+			if color == inputColor {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("missing color %v from palette", color)
+		}
+	}
+}
